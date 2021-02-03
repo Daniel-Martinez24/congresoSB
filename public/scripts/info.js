3bddf3525ac5b843862e489813b3da8b0567e8a3
Daniel Martinez Diaz <dan24diaz@gmail.com>
2021-02-03 16:04:15 -0600

    añadido archivo con informacion de talleres y charla con informacion falsa

---
+++ public/scripts/info.js
@@ -0,0 +1,50 @@
+const talleres = [
+    {
+        titulo: 'ndasondoasdnasodna',
+        tallerista: 'asdasdasdasdasdasdasdasd',
+        fechaUno: 'asdasdasdasdasdasdasdasd',
+        fechaDos: 'asdasdasdasdasdasdasdasd',
+        verMas: '#'
+    },
+    {
+        titulo: 'ndasondoasdnasodna',
+        tallerista: 'asdasdasdasdasdasdasdasd',
+        fechaUno: 'asdasdasdasdasdasdasdasd',
+        fechaDos: 'asdasdasdasdasdasdasdasd',
+        verMas: '#'
+    },
+    {
+        titulo: 'ndasondoasdnasodna',
+        tallerista: 'asdasdasdasdasdasdasdasd',
+        fechaUno: 'asdasdasdasdasdasdasdasd',
+        fechaDos: 'asdasdasdasdasdasdasdasd',
+        verMas: '#'
+    }
+]
+
+const charlas = [
+    {
+        titulo: 'ndasondoasdnasodna',
+        tallerista: 'asdasdasdasdasdasdasdasd',
+        fechaUno: 'asdasdasdasdasdasdasdasd',
+        fechaDos: 'asdasdasdasdasdasdasdasd',
+        verMas: '#'
+    },
+    {
+        titulo: 'ndasondoasdnasodna',
+        tallerista: 'asdasdasdasdasdasdasdasd',
+        fechaUno: 'asdasdasdasdasdasdasdasd',
+        fechaDos: 'asdasdasdasdasdasdasdasd',
+        verMas: '#'
+    },
+    {
+        titulo: 'ndasondoasdnasodna',
+        tallerista: 'asdasdasdasdasdasdasdasd',
+        fechaUno: 'asdasdasdasdasdasdasdasd',
+        fechaDos: 'asdasdasdasdasdasdasdasd',
+        verMas: '#'
+    }
+]
+
+
+const informacion = {talleres, charlas}
