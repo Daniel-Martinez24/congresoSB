b42f5633cf8c2ceb5d2c6910b445cd226383ebf3
Daniel Martinez Diaz <dan24diaz@gmail.com>
2021-02-04 13:53:23 -0600

    Añadidos primeros datos de tallleres

--- public/scripts/info.js
+++ public/scripts/info.js
@@ -1,25 +1,54 @@
 const talleres = [
     {
-        titulo: 'ndasondoasdnasodna',
-        tallerista: 'asdasdasdasdasdasdasdasd',
-        fechaUno: 'asdasdasdasdasdasdasdasd',
-        fechaDos: 'asdasdasdasdasdasdasdasd',
+        titulo: 'Taller de principios básicos de análisis filogenético',
+        tallerista: 'Dr. Luis José Delaye Arredondo',
+        fechaUno: 'dai 1 17:05',
+        fechaDos: 'dia 2 17:05',
         verMas: '#'
     },
     {
-        titulo: 'ndasondoasdnasodna',
-        tallerista: 'asdasdasdasdasdasdasdasd',
-        fechaUno: 'asdasdasdasdasdasdasdasd',
-        fechaDos: 'asdasdasdasdasdasdasdasd',
+        titulo: 'Taller de Biología Sintética',
+        tallerista: 'Dr. Luis Joel Figueroa Yañez',
+        fechaUno: 'dia 1 18:25',
+        fechaDos: 'dia 2 18:25',
+        fechaTres: 'dia 3 17:05',
         verMas: '#'
     },
     {
-        titulo: 'ndasondoasdnasodna',
-        tallerista: 'asdasdasdasdasdasdasdasd',
-        fechaUno: 'asdasdasdasdasdasdasdasd',
-        fechaDos: 'asdasdasdasdasdasdasdasd',
+        titulo: 'Taller básico de fotografía',
+        tallerista: 'Kassandra Muros',
+        fechaUno: 'dia 2 14:20',
+        fechaDos: 'dia 3 13:15',
         verMas: '#'
-    }
+    },
+    {
+        titulo: 'Taller de Genética',
+        tallerista: 'Ing. Miguel Fernando Espitia Verdugo',
+        fechaUno: 'dia 3 18:25',
+        fechaDos: 'dia 5 18:25',
+        verMas: '#'
+    },
+    {
+        titulo: 'Taller de IA para ciencias biológicas',
+        tallerista: 'Lic. Rodolfo Ferro',
+        fechaUno: 'lunes 17:05',
+        fechaDos: 'null',
+        verMas: '#'
+    },
+    {
+        titulo: 'Taller de divulgación científica',
+        tallerista: 'Null',
+        fechaUno: 'dia 5 17:05',
+        fechaDos: 'dia 7 :16:25',
+        verMas: '#'
+    },
+    {
+        titulo: 'Taller de evolución bacteriana',
+        tallerista: 'Mtra. Paz Catalina Gutiérrez Rosas',
+        fechaUno: 'dia 6 17:05',
+        fechaDos: 'null',
+        verMas: '#'
+    },
 ]
 
 const charlas = [
