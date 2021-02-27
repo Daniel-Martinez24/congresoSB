0f163d6c3a34503444d2943098b454a5c7302e23
Daniel Martinez Diaz <dan24diaz@gmail.com>
2021-02-26 21:42:28 -0600

    se actuaaliza la info  en taller

--- public/scripts/rutasTalleres.js
+++ public/scripts/rutasTalleres.js
@@ -10,6 +10,51 @@ function getParameterByName(name) {
 }
 
 
+
 var tallerID = getParameterByName('tallerID');
 
-console.log(tallerID);
+const idExiste = (infoTaller) => {
+    document.getElementById("titulo-taller").innerText = infoTaller.titulo;
+    document.getElementById("tallerista").innerText = 'Por '+ infoTaller.tallerista;
+    document.getElementById("infoTaller").innerText = infoTaller.infoTaller;
+
+
+    document.getElementById("horaInicio").innerText = infoTaller.fechas[0];
+    document.getElementById("horaFin").innerText = infoTaller.fechas[1];
+
+    ReactDOM.render(<RequistosTaller requisitos={infoTaller.requisitos} /> , document.getElementById('requistos'));
+
+
+
+}
+
+const encontrarID = () => {
+    let seEncontro = false ;
+    informacion.talleres.map((tallerdelist) => {
+        if (tallerID === tallerdelist.id) {
+            seEncontro = true;
+            idExiste(tallerdelist);
+        }
+    })
+    return seEncontro
+}
+
+const escribirInformacion = (encontrarID) => {
+    if (!encontrarID()) window.history.back();
+}
+
+escribirInformacion(encontrarID);
+
+function RequistosTaller(props) {
+   
+    const listaRequisitos = props.requisitos;
+    const Lrequistio = listaRequisitos.map((requisito) => 
+        <li>{requisito}</li>    
+    );
+    return (
+        <ul className="list"> 
+            {Lrequistio}
+        </ul>
+    );
+}
+
